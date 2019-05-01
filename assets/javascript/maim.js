ac62000c38319ed077cc1cc20b1bcfbdc95fb1da
unknown <stanmozolevskiy@gmail.com>
2019-05-01 08:05:26 -0400

    finishe at work

--- assets/javascript/maim.js
+++ assets/javascript/maim.js
@@ -1,5 +1,5 @@
 //Show default gifs "Rackoon"
-$( document ).ready(showGif($("#default")))
+$(document).ready(showGif($("#default")))
 // Adding new button on screen
 function newCard() {
     if ($("#input-for-gif").val().length > 0) {
@@ -17,7 +17,7 @@ function newCard() {
 function showGif(buttonData) {
     const queryURL = "https://api.tenor.com/v1/search?q=" +
         buttonData.attr('data-gif') + "&key=LHBU6VVIEG68";
-//axios call
+    //axios call
     axios.get(queryURL)
         .then((response) => {
             // Storing an array of response in the frontEndData variable
@@ -29,7 +29,7 @@ function showGif(buttonData) {
                 // Creating a div for the gif
                 let gifDiv = $("<div>").attr("class", "card")
                 // Creating an image tag
-                let gifImage = $("<img>").attr("class", "gif-image").attr("class", "card-img-top");
+                let gifImage = $("<img>").attr("class", "gif-image").attr("class", "card-img-top hl");
                 gifImage.attr("gifurl", frontEndData[i].media[0].gif.url);
                 // Giving the image tag an src attribute of a proprty pulled off the
                 gifImage.attr("src", frontEndData[i].media[0].gif.url);
@@ -38,6 +38,37 @@ function showGif(buttonData) {
                 // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
                 $("#gifs-appear-here").prepend(gifDiv)
             }
+        }).then(() => {
+            //
+            // //manipulate with input
+            // const blurnput = document.getElementById('blur');
+            // const output = document.querySelectorAll('img');
+            // const blur = 'filter: blur()'
+            const inputs = document.querySelectorAll(' input')
+            console.log(inputs)
+            function handleUpdates() {
+                const sufix = "px"
+                console.log(this.name)
+                document.documentElement.style.setProperty(`--${this.name}`, this.value + sufix)
+            }
+            inputs.forEach(input => input.addEventListener('change',handleUpdates ))
+            inputs.forEach(input => input.addEventListener('mousemove',handleUpdates ))
+            // // go throught all selected images (need to fing more productive way)
+            // console.log(blurnput)
+            // for (let i = 0; i < output.length; i++) {
+
+            //     function setDefaultState() {
+            //         output[i].style.blur = blurnput.value + 'px';
+            //         console.log(output[i].style.blur)
+
+            //     }
+            //     document.addEventListener('change', function () {
+            //         setDefaultState();
+            //     });
+            //     // document.addEventListener('mousemove', function () {
+            //     //     setDefaultState();
+            //     // });
+            // }
         })
         .catch(function (error) {
             console.log(error);
@@ -52,6 +83,7 @@ $(document).on('keyup', function (e) {
 });
 // event handler for klicking on button
 $(document).on("click", '.redyButton', function () { showGif($(this)) })
+
 
 
 
