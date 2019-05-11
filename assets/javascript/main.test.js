6e5d29da3e0eac149cc53a1e3d255568cd71c168
unknown <stanmozolevskiy@gmail.com>
2019-05-11 18:25:23 -0400

    testcafe edded

--- assets/javascript/main.test.js
+++ assets/javascript/main.test.js
@@ -1,3 +1,29 @@
-import  main from "./main.js"
+import { Selector } from 'testcafe';
 
-console.log(main)
+fixture `Populate the Gifs`
+    .page `https://stanmozolevskiy.github.io/GifTastic/index.html`;
+
+test('Populate the Gifs', async t => {
+    await t
+        .typeText(Selector('#input-for-gif'), 'test')
+        .click(Selector('#addNewButton'));
+
+    const Gifcard = Selector('.card').find('.card-img-top.hl');
+
+    await t
+        .click(Selector('#blur'));
+});
+
+test('Populate Button', async t => {
+    await t
+        .typeText(Selector('#input-for-gif'), 'test')
+        .pressKey('enter');
+
+    const button = Selector('button').withText('Test');
+
+    await t
+        .drag(Selector('#border'), -148, 29, {
+            offsetX: 196,
+            offsetY: 12
+        });
+});
